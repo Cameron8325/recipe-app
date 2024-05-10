6b31ae1cde9defcb5f10a62e5435efb2cc8caffc
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-05-10 17:14:58 -0400

    created templates, views, and added imagefields

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, RecipeListView, recipe_detail
 
 app_name = 'recipes'
 
 urlpatterns = [
-   path('', home),
+    path('', home, name='home'),
+    path('recipes/', RecipeListView.as_view(), name='recipes_list'),
+    path('recipes/<int:recipe_id>/', recipe_detail, name='recipe_detail'),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .models import Recipe
 
 def home(request):
-  return render(request, 'recipes/recipes_home.html')
+    return render(request, 'recipes/recipes_home.html')
+
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipes/recipes_list.html'
+    context_object_name = 'recipes'
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
