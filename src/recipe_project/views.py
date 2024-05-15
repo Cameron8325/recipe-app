d2e6b1bbf9a804b68fd75c66ac4f9614d40f18fe
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-05-15 14:26:26 -0400

    Added authentication and view protection

---
+++ src/recipe_project/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect            
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm   
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'success.html')
+
+
+
+def login_view(request):                           
+   error_message = None   
+                              
+   form = AuthenticationForm()                            
+
+   if request.method == 'POST':       
+       #read the data sent by the form via POST request                   
+       form =AuthenticationForm(data=request.POST)
+
+       #check if form is valid
+       if form.is_valid():                                
+           username=form.cleaned_data.get('username') 
+           password = form.cleaned_data.get('password')
+
+           #use Django authenticate function to validate the user
+           user=authenticate(username=username, password=password)
+           if user is not None:                    
+               login(request, user)                
+               return redirect('recipes:home')
+       else:                                               
+           error_message ='ooops.. something went wrong'   
+
+   
+   context ={                                             
+       'form': form,
+       'error_message': error_message 
+   }
+   
+   return render(request, 'auth/login.html', context)
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'auth/success.html')
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Recipe
@@ -6,11 +8,12 @@ def home(request):
     featured_recipes = Recipe.objects.order_by('?')[:3]
     return render(request, 'recipes/recipes_home.html', {'featured_recipes': featured_recipes})
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'recipes/recipes_list.html'
     context_object_name = 'recipes'
 
+@login_required
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, pk=recipe_id)
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
