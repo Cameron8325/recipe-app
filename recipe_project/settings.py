4ac5eaa8bbf30c574b463bb415a8264bb97f7216
Cameron8325 <140201247+Cameron8325@users.noreply.github.com>
2024-05-26 19:58:46 -0400

    Fixed js error

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "frozen-river-49992-8fdfadf5baa0.herokuapp.com"]
 
